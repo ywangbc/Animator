fc7f1c8d9a1ab78c4150c5bda169c803afd90760
ywangbc@gmail.com <9q9q99QQ>
2014-05-04 12:30:30 +0800

    MoreFiles

---
+++ catmullromevaluator.h
@@ -0,0 +1,19 @@
+#ifndef INCLUDED_CATMULLROM_EVALUATOR_H
+#define INCLUDED_CATMULLROM_EVALUATOR_H
+
+#pragma warning(disable : 4786)  
+
+#include "CurveEvaluator.h"
+
+//using namespace std;
+
+class CatmullRomEvaluator : public CurveEvaluator
+{
+public:
+	void evaluateCurve(const std::vector<Point>& ptvCtrlPts,
+		std::vector<Point>& ptvEvaluatedCurvePts,
+		const float& fAniLength,
+		const bool& bWrap) const;
+};
+
+#endif
