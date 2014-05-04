51f24f2afa11c3c6b926a216095008d4a6c81400
ywangbc@gmail.com <9q9q99QQ>
2014-05-04 23:32:34 +0800

    BezierCurveCompleted

--- beziercurveevaluaror.cpp
+++ beziercurveevaluaror.cpp
@@ -1,5 +1,6 @@
 #include "beziercurveevaluator.h"
 #include <assert.h>
+#include <iostream>
 
 Point BezierCurveEvaluator::getAvg(Point p1, Point p2) const
 {
@@ -8,12 +9,39 @@ Point BezierCurveEvaluator::getAvg(Point p1, Point p2) const
 
 
 
-void BezierCurveEvaluator::displayBezier(const Point v0, const Point v1, const Point v2, const Point v3, std::vector<Point>& EvaluatedPoints) const
+void BezierCurveEvaluator::displayBezier(const Point v0, const Point v1, const Point v2, const Point v3, std::vector<Point>& EvaluatedPoints, float animateLength) const
 {
 	if (flatEnough(v0, v1, v2, v3))
 	{
-		EvaluatedPoints.push_back(v0);
-		EvaluatedPoints.push_back(v3);
+		Point v0New = v0;
+		Point v3New = v3;
+		if (v0.x > animateLength)
+		{
+			v0New.x = v0.x-animateLength;
+		}
+		if (v3New.x > animateLength)
+		{
+			v3New.x = v3.x - animateLength;
+		}
+
+		if ((v0.x - animateLength)*(v3.x - animateLength) < 0)
+		{
+			EvaluatedPoints.push_back(v0New);
+			EvaluatedPoints.insert(EvaluatedPoints.begin(),v3New);
+		}
+		else if ((v0.x - animateLength)>0 && (v3.x - animateLength) > 0)
+		{
+			EvaluatedPoints.insert(++EvaluatedPoints.begin(), v0New);
+			EvaluatedPoints.insert(++EvaluatedPoints.begin(), v3New);
+		}
+	
+		else
+		{
+			EvaluatedPoints.push_back(v0New);
+			EvaluatedPoints.push_back(v3New);
+		}
+
+		
 	}
 	else
 	{
@@ -24,8 +52,8 @@ void BezierCurveEvaluator::displayBezier(const Point v0, const Point v1, const P
 		Point v1NewNew = getAvg(v1New, v2New);
 		Point Qu = getAvg(v0NewNew,v1NewNew);
 
-		displayBezier(v0, v0New, v0NewNew, Qu, EvaluatedPoints);
-		displayBezier(Qu, v1NewNew, v2New, v3, EvaluatedPoints);
+		displayBezier(v0, v0New, v0NewNew, Qu, EvaluatedPoints, animateLength);
+		displayBezier(Qu, v1NewNew, v2New, v3, EvaluatedPoints, animateLength);
 	}
 }
 bool BezierCurveEvaluator::flatEnough(const Point v0, const Point v1, const Point v2, const Point v3) const
@@ -34,6 +62,10 @@ bool BezierCurveEvaluator::flatEnough(const Point v0, const Point v1, const Poin
 	float dist2 = v1.distance(v2);
 	float dist3 = v2.distance(v3);
 	float dist4 = v0.distance(v3);
+	if (dist4 < DBL_EPSILON)
+	{
+		dist4 = DBL_EPSILON;
+	}
 
 	if (((dist1 + dist2 + dist3) / dist4)<(1 + FLATEPSILON))
 	{
@@ -53,21 +85,16 @@ void BezierCurveEvaluator::evaluateCurve(const std::vector<Point>& ptvCtrlPts,
 	//First we clear the memory of previous render
 	ptvEvaluatedCurvePts.clear();
 
-
-
 	int iCtrlPtCount = ptvCtrlPts.size();
-
 	Point v0;
-	Point v1; 
+	Point v1;
 	Point v2;
 	Point v3;
 
 	float x = 0.0;
 	float y1;
+
 	//Dvide the points into groups of four
-
-	//ptvEvaluatedCurvePts.push_back(ptvCtrlPts[0]);
-
 	for (int groupNum = 0; groupNum < iCtrlPtCount; groupNum += STEPLENGTH)
 	{
 		int numPointLeft = iCtrlPtCount - groupNum;
@@ -77,50 +104,49 @@ void BezierCurveEvaluator::evaluateCurve(const std::vector<Point>& ptvCtrlPts,
 			v1 = ptvCtrlPts[groupNum + 1];
 			v2 = ptvCtrlPts[groupNum + 2];
 			v3 = ptvCtrlPts[groupNum + 3];
-			displayBezier(v0, v1, v2, v3, ptvEvaluatedCurvePts);
+			displayBezier(v0, v1, v2, v3, ptvEvaluatedCurvePts, fAniLength);
 		}
 		else
 		{
-			for (int i = 0; i < numPointLeft; i++)
+			if (bWrap&&numPointLeft == 3)
 			{
-				ptvEvaluatedCurvePts.push_back(ptvCtrlPts[groupNum + i]);
+				v0 = ptvCtrlPts[groupNum];
+				v1 = ptvCtrlPts[groupNum + 1];
+				v2 = ptvCtrlPts[groupNum + 2];
+				v3 = ptvCtrlPts[0];
+				v3.x = v3.x + fAniLength;
+				displayBezier(v0, v1, v2, v3, ptvEvaluatedCurvePts, fAniLength);
 			}
+			else
+			{
+				for (int i = 0; i < numPointLeft; i++)
+				{
+					ptvEvaluatedCurvePts.push_back(ptvCtrlPts[groupNum + i]);
+				}
+			}
+
 		}
-		
-		
-		y1 = ptvCtrlPts[0].y;
-
-		ptvEvaluatedCurvePts.push_back(Point(x, y1));
-
-		x = fAniLength;
-
-		float y2;
-		y2 = y1;
-		ptvEvaluatedCurvePts.push_back(Point(x, y2));
 	}
 
-	/*
-	float x = 0.0;
-	float y1;
+	int evaluatePtCount = ptvEvaluatedCurvePts.size();
 
-	if (bWrap) {
-		// if wrapping is on, interpolate the y value at xmin and
-		// xmax so that the slopes of the lines adjacent to the
-		// wraparound are equal.
-
-		if ((ptvCtrlPts[0].x + fAniLength) - ptvCtrlPts[iCtrlPtCount - 1].x > 0.0f) {
-			y1 = (ptvCtrlPts[0].y * (fAniLength - ptvCtrlPts[iCtrlPtCount - 1].x) +
-				ptvCtrlPts[iCtrlPtCount - 1].y * ptvCtrlPts[0].x) /
-				(ptvCtrlPts[0].x + fAniLength - ptvCtrlPts[iCtrlPtCount - 1].x);
+	if (bWrap)
+	{
+		if ((ptvEvaluatedCurvePts[0].x + fAniLength) - ptvEvaluatedCurvePts[evaluatePtCount - 1].x > 0.0f) {
+			y1 = (ptvEvaluatedCurvePts[0].y * (fAniLength - ptvEvaluatedCurvePts[evaluatePtCount - 1].x) +
+				ptvEvaluatedCurvePts[evaluatePtCount - 1].y * ptvEvaluatedCurvePts[0].x) /
+				(ptvEvaluatedCurvePts[0].x + fAniLength - ptvEvaluatedCurvePts[evaluatePtCount - 1].x);
 		}
 		else
-			y1 = ptvCtrlPts[0].y;
+		{
+			y1 = ptvEvaluatedCurvePts[0].y;
+		}
 	}
 	else {
 		// if wrapping is off, make the first and last segments of
 		// the curve horizontal.
 
-		y1 = ptvCtrlPts[0].y;
+		y1 = ptvEvaluatedCurvePts[0].y;
 	}
 
 	ptvEvaluatedCurvePts.push_back(Point(x, y1));
@@ -131,8 +157,8 @@ void BezierCurveEvaluator::evaluateCurve(const std::vector<Point>& ptvCtrlPts,
 	if (bWrap)
 		y2 = y1;
 	else
-		y2 = ptvCtrlPts[iCtrlPtCount - 1].y;
+		y2 = ptvEvaluatedCurvePts[evaluatePtCount - 1].y;
 
 	ptvEvaluatedCurvePts.push_back(Point(x, y2));
-	*/
+
 }
