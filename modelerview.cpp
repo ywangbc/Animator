c1be9f967864be6e16481bee7f4c622bfffb4278
NwinterC <largec4@gmail.com>
2014-05-09 21:02:04 +0800

    Clip plane

--- modelerview.cpp
+++ modelerview.cpp
@@ -119,11 +119,7 @@ void ModelerView::draw()
     glLightfv( GL_LIGHT1, GL_DIFFUSE, lightDiffuse1 );
 
 	// If particle system exists, draw it
-	ParticleSystem *ps = ModelerApplication::Instance()->GetParticleSystem();
-	if (ps != NULL) {
-		ps->computeForcesAndUpdateParticles(t);
-		ps->drawParticles(t);
-	}
+	
 }
 
 
